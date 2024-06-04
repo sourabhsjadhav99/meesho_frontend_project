7b2ef52d8b4ec6b6dd8fbac77f60078f80526da4
mdshadab998998 <mdshadabkhan4256@gmail.com>
2024-06-04 23:16:10 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './index.css'; // Ensure this import is present
 import Home from './pages/home/Home';
 import SingleProduct from './pages/product/SingleProduct';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './components/signup/signup';
 import Footer from './components/Footer';
+import Navbar from './components/Header/Header';
+
 
 function App() {
   return (
     <BrowserRouter>
-
+      <Navbar />
        <Routes>
              <Route path="/" element={<Home />} />
+             <Route path="/signup" element={<Signup />} />
              <Route path="/:id" element={<SingleProduct/>} />
 
          </Routes>
--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from "./redux/store"
+import { AuthProvider } from '../src/components/signup/AuthContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <AuthProvider>
+
+
     <App />
+    </AuthProvider>
+
   </Provider>,
 );
 
